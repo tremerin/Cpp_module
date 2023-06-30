4c03f26b38b8a5d5463a2d22ce2dbb37dc90b2c7
tremerin <tremerin13@hotmail.com>
2023-06-30 12:43:30 +0200

    headers

--- Module04/ex00/main.cpp
+++ Module04/ex00/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fgalan-r <fgalan-r@student.42.fr>          +#+  +:+       +#+        */
+/*   By: fgalan-r <fgalan-r@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/26 19:40:58 by fgalan-r          #+#    #+#             */
-/*   Updated: 2023/06/26 19:40:59 by fgalan-r         ###   ########.fr       */
+/*   Updated: 2023/06/30 12:37:21 by fgalan-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ const Animal* j = new Dog();
 const Animal* i = new Cat();
 std::cout << j->getType() << " " << std::endl;
 std::cout << i->getType() << " " << std::endl;
-i->makeSound(); //will output the cat sound!
+i->makeSound();
 j->makeSound();
 meta->makeSound();
 delete i;
