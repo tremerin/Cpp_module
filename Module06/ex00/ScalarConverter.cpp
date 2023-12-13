b1c6a7cdf8635804460bed541fb378fe5adf2327
tremerin <tremerin13@hotmail.com>
2023-12-13 20:12:43 +0100

    module6 ex00

--- Module06/ex00/ScalarConverter.cpp
+++ Module06/ex00/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: fgalan-r <fgalan-r@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/12 17:31:38 by fgalan-r          #+#    #+#             */
-/*   Updated: 2023/12/12 18:17:30 by fgalan-r         ###   ########.fr       */
+/*   Updated: 2023/12/13 19:35:40 by fgalan-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,20 @@ ScalarConverter::ScalarConverter()
 	std::cout << "Constructor is called" << std::endl;
 }
 
+ScalarConverter::ScalarConverter(const ScalarConverter & src)
+{
+	(void)src;
+}
+
 ScalarConverter::~ScalarConverter()
 {
 	std::cout << "Destructor is called" << std::endl;
+}
+
+ScalarConverter & ScalarConverter::operator=(const ScalarConverter & rhs)
+{
+	(void)rhs;
+	return *this;
 }
 
 void ScalarConverter::Convert(const std::string)
